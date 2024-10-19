60fb80dcffbf4f7c4ac42fc7b801b9342093160a
Satya-Prakash-Dwivedi <dwivediprakashsatya36@gmail.com>
2024-10-19 19:05:44 +0530

    Handling models and urls in Django

--- chaiaurdjango/chai/views.py
+++ chaiaurdjango/chai/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import chaiVariety
 
 # Create your views here.
 
 def all_chai(request):
-    return render(request, 'chai/all_chai.html')
+    chais = chaiVariety.objects.all()
+    return render(request, 'chai/all_chai.html', {'chais' : chais})
+
+def chai_detail(request, chai_id):
+    chai = get_object_or_404(chaiVariety, pk=chai_id)
+    return(request, 'chai/chai_detail.html', {'chai' : chai})
